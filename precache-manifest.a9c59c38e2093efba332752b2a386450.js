2c571e42979c803eef79d482b38956b01d89e5bb
PavloPantus <mr.pantus@gmail.com>
2020-01-08 23:43:35 +0200

    Updates

---
+++ precache-manifest.a9c59c38e2093efba332752b2a386450.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e069266b487ac247843c13ea0aea7cc",
+    "url": "/react_redux-list-of-todos/index.html"
+  },
+  {
+    "revision": "e864e510c9f907ad44a7",
+    "url": "/react_redux-list-of-todos/static/css/main.0e48773b.chunk.css"
+  },
+  {
+    "revision": "d89ba9468a4f14f629ec",
+    "url": "/react_redux-list-of-todos/static/js/2.a357c283.chunk.js"
+  },
+  {
+    "revision": "e864e510c9f907ad44a7",
+    "url": "/react_redux-list-of-todos/static/js/main.7573a49d.chunk.js"
+  },
+  {
+    "revision": "37dc44999646af065e8e",
+    "url": "/react_redux-list-of-todos/static/js/runtime~main.054cc81e.js"
+  }
+]);
